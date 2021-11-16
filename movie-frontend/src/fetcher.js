6a267666db391f5935abe7385b639bdb331a9055
HappyHacking12345 <HappyHacking12345@gmail.com>
2021-11-16 15:18:08 -0500

    completed signin signup and skeleton for the frontend

---
+++ movie-frontend/src/fetcher.js
@@ -0,0 +1,85 @@
+import config from './config.json'
+
+export const login = (credential) => {
+    const loginUrl = `/login?username=${credential.username}&password=${credential.password}`;
+
+    return fetch(loginUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+    }).then((response) => {
+        if (response.status < 200 || response.status >= 300) {
+            throw Error("Fail to log in");
+        }
+    });
+};
+
+export const signup = (data) => {
+    const signupUrl = "/signup";
+
+    return fetch(signupUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    }).then((response) => {
+        if (response.status < 200 || response.status >= 300) {
+            throw Error("Fail to sign up");
+        }
+    });
+};
+
+
+const getAllMatches = async (page, pagesize, league) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/matches/${league}?page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getAllPlayers = async (page, pagesize) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/players?page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getMatch = async (id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/match?id=${id}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getPlayer = async (id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/player?id=${id}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getMatchSearch = async (home, away, page, pagesize) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/matches?Home=${home}&Away=${away}&page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getPlayerSearch = async (name, nationality, club, rating_high, rating_low, pot_high, pot_low, page, pagesize) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/players?Name=${name}&Nationality=${nationality}&Club=${club}&RatingLow=${rating_low}&RatingHigh=${rating_high}&PotentialHigh=${pot_high}&PotentialLow=${pot_low}&page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+export {
+    getAllMatches,
+    getAllPlayers,
+    getMatch,
+    getPlayer,
+    getMatchSearch,
+    getPlayerSearch
+}
